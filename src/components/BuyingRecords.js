da7ae1f74936d4bad1803bea0d30d9d59fa1c252
Rana Ahsan Ansar <asn.cs21@gmail.com>
2023-04-20 15:43:46 +0500

    Display Table Setting

--- src/components/BuyingRecords.js
+++ src/components/BuyingRecords.js
@@ -8,16 +8,16 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Box, Typography } from '@mui/material';
 
-function createData(society , block , propertyId , shares , amountPerShare , seller, buyer , status , contract) {
-  return { society , block , propertyId , shares , amountPerShare , seller, buyer , status , contract};
+function createData(society , block , propertyId , shares , amountPerShare , seller, buyer , buyerSing ,  status , contract) {
+  return { society , block , propertyId , shares , amountPerShare , seller, buyer , buyerSing , status , contract};
 }
 
 const rows = [
-  createData('Bahria' , 'A' , '786' , 20 , 5000 , 'Ahsan Ansar'  , 'Talal' , true , '9X00390g0s2094099430gh0294509dfgh0'  ),
-  createData('Bahria' , 'B' , '786' , 20 , 5000 , 'Ahsan Ansar'  , 'Talal' , true , '9X00390g0s2094099430gh0294509dfgh0'  ),
-  createData('Bahria' , 'X' , '786' , 20 , 5000 , 'Ahsan Ansar'  , 'Talal' , false , '9X00390g0s2094099430gh0294509dfgh0'  ),
-  createData('Bahria' , 'Y' , '786' , 20 , 5000 , 'Ahsan Ansar'  , 'Talal' , true , '9X00390g0s2094099430gh0294509dfgh0'  ),
-  createData('Bahria' , 'Z' , '786' , 20 , 5000 , 'Ahsan Ansar'  , 'Talal' , false , '9X00390g0s2094099430gh0294509dfgh0'  ),
+  createData('Bahria' , 'A' , '786' , 20 , 5000 , 'Ahsan Ansar'  , 'Talal' , true , true , '9X00390g0s2094099430gh0294509dfgh0'  ),
+  createData('Bahria' , 'B' , '786' , 20 , 5000 , 'Ahsan Ansar'  , 'Talal' , true , true ,'9X00390g0s2094099430gh0294509dfgh0'  ),
+  createData('Bahria' , 'X' , '786' , 20 , 5000 , 'Ahsan Ansar'  , 'Talal' , false ,false , '9X00390g0s2094099430gh0294509dfgh0'  ),
+  createData('Bahria' , 'Y' , '786' , 20 , 5000 , 'Ahsan Ansar'  , 'Talal' , true , true ,'9X00390g0s2094099430gh0294509dfgh0'  ),
+  createData('Bahria' , 'Z' , '786' , 20 , 5000 , 'Ahsan Ansar'  , 'Talal' , false , false ,'9X00390g0s2094099430gh0294509dfgh0'  ),
 ];
 const BuyingRecords = () => {
     return (
@@ -32,6 +32,7 @@ const BuyingRecords = () => {
                 <TableCell sx={{ fontWeight: 'bold' }} align="center">Amount per Share</TableCell>
                 <TableCell sx={{ fontWeight: 'bold' }} align="center">Seller</TableCell>
                 <TableCell sx={{ fontWeight: 'bold' }} align="center">Buyer</TableCell>
+                <TableCell sx={{ fontWeight: 'bold' }} align="center">Buyer Sing</TableCell>
                 <TableCell sx={{ fontWeight: 'bold' }} align="center">Status</TableCell>
                 <TableCell sx={{ fontWeight: 'bold' }} align="center">Contract</TableCell>
               </TableRow>
@@ -50,6 +51,7 @@ const BuyingRecords = () => {
                   <TableCell align="right">{row.amountPerShare}</TableCell>
                   <TableCell align="right">{row.seller}</TableCell>
                   <TableCell align="right">{row.buyer}</TableCell>
+                  <TableCell align="right">{row.buyerSing ?( <Box><Typography color='green' >Singed</Typography></Box> ) : ( <Box><Typography color='red' >Pending</Typography></Box> )}</TableCell>
                   <TableCell align="right">{row.status ?( <Box><Typography color='green' >Approved</Typography></Box> ) : ( <Box><Typography color='red' >Pending</Typography></Box> )}</TableCell>
                   <TableCell align="right">{row.contract}</TableCell>
                 </TableRow>
